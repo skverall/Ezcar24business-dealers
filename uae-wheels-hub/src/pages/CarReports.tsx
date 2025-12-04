af411848b02178cd8d17fca624ac98d497e6231d
AydMaxx <167026061+skverall@users.noreply.github.com>
2025-12-05 01:05:26 +0400

    ир

--- uae-wheels-hub/src/pages/CarReports.tsx
+++ uae-wheels-hub/src/pages/CarReports.tsx
@@ -2,7 +2,6 @@
 import { useTranslation } from "react-i18next";
 import { useSearchParams } from "react-router-dom";
 import Header from "@/components/Header";
-import Footer from "@/components/Footer";
 import CarInspectionReport from "@/components/CarInspectionReport";
 
 const CarReports = () => {
@@ -16,7 +15,6 @@ const CarReports = () => {
             <main className="flex-grow pt-24 pb-12 px-4 bg-[#0A0A0A]">
                 <CarInspectionReport reportId={reportId} />
             </main>
-            <Footer />
         </div>
     );
 };
